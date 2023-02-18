aecaa7e02bf62c1bec9a4f169d115d8cc5ca563f
David Cervantes <jesus_d_cc@hotmail.com>
2023-02-18 10:50:19 -0700

    Buscando

--- ListasSimplementeLigadas/Lista.cs
+++ ListasSimplementeLigadas/Lista.cs
@@ -14,6 +14,40 @@ namespace ListasSimplementeLigadas
         {
             nodoInicial= new Nodo();
         }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == null)
+            {
+                return true;
+            }
+            return false;
+            //return nodoInicial.Siguiente == null;
 
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = null;
+        }
+        public string Recorrer()
+        {
+            string datos = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                datos += nodoActual.Valor + "\n";
+            }
+            return datos;
+        }
+        public void Agregar(string valor)
+        {
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+            Nodo nodoNuevo = new Nodo(valor);
+            nodoActual.Siguiente= nodoNuevo;
+        }
     }
 }
